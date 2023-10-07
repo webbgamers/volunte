9ab33086feb63d8e63c036cbfc8814eb50aa977f
Calvin Brosch <53014638+CBrosch0@users.noreply.github.com>
2023-10-07 07:15:27 -0400

    Started swap between login and register

--- volunte-frontend/src/App.jsx
+++ volunte-frontend/src/App.jsx
@@ -1,36 +1,122 @@
 import { useState } from 'react'
 import './App.css'
 
-function LoginForm() {
-  return (
-    <>
-      <h1>Volunte</h1>
 
-      <div>
-        Username: 
-        <input name = "Username"/>
-
-      <br/>
-      <br/>
-
-        Password: 
-        <input name = "Password"/>
-      </div>
-      
-      <br/>
-      <br/>
-
-      <button type="submit">Log in</button>
-    </>
-  )
-}
 
 function App() {
   const [count, setCount] = useState(0)
 
+  var page;
+  var signedIn = false, needLogin = true, needRegister = false;
+  var b = 2;
+
+  function LoginForm() {
+    return (
+      <>
+        <h1>Volunte</h1>
+
+        <div>
+          Username:
+          <input name="Username" />
+
+          <br />
+          <br />
+
+          Password:
+          <input name="Password" />
+        </div>
+
+        <br />
+        <br />
+
+        <button type="submit" onClick={Login}>Log in</button>
+      </>
+    )
+  }
+
+  function Login() {
+    signedIn = true;
+    needLogin = false;
+  }
+
+  function RegisterForm() {
+    return (
+      <>
+        <h1>Volunte</h1>
+
+        <div>
+
+          First name:
+          <input name="First name" />
+
+          <br />
+          <br />
+
+          Last name:
+          <input name="Last name" />
+
+          <br />
+          <br />
+
+          Username:
+          <input name="Username" />
+
+          <br />
+          <br />
+
+          Password:
+          <input name="Password" />
+
+          <br />
+          <br />
+
+          Re-enter Password:
+          <input name="Password" />
+        </div>
+
+        <br />
+        <br />
+
+        <button type="submit" onClick={Register}>Register</button>
+      </>
+    )
+  }
+
+  function Register() {
+    signedIn = true;
+    needRegister = false;
+  }
+
+  function LoginRegisterSwap() {
+    
+    b = 6;
+    if (!needLogin) {
+    alert('!needLogin');
+
+      needLogin = true;
+      needRegister = false;
+      b = 4;
+    } else {
+    alert('needLogin');
+
+      needLogin = false;
+      needRegister = true;
+      b = 5;
+    }
+  }
+
+  if (!signedIn && needLogin) {
+    page = <LoginForm />
+  } else if (!signedIn && needRegister) {
+    page = <RegisterForm />
+  }
+
   return (
     <>
-      <LoginForm/>
+      <button onClick={LoginRegisterSwap}>Swap</button>
+      {page}
+      {needLogin}
+      {b}
     </>
   )
 }
