b47a66238160869bd84be1919b5f0bf4a3be5aa4
Calvin Brosch <53014638+CBrosch0@users.noreply.github.com>
2023-10-07 05:46:39 -0400

    Laid out rough login page

--- volunte-frontend/src/App.jsx
+++ volunte-frontend/src/App.jsx
@@ -8,25 +8,20 @@ function App() {
 
   return (
     <>
+      <h1>Volunte</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Username: 
+        <input name = "Username"/>
+
+      <br/>
+      <br/>
+
+        Password: 
+        <input name = "Password"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        This login is a work in progress
       </p>
     </>
   )
