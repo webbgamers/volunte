f22931800699b24662f9fe3199a2b38e5a5d064a
Ivan Rosyaykin <evanmico222@gmail.com>
2023-10-08 08:15:10 -0400

    Added Styling for edit user

--- volunte-frontend/src/App.jsx
+++ volunte-frontend/src/App.jsx
@@ -190,11 +190,37 @@ function App() {
   function userInfo() {
     return (
       <>
-        <h1>User Info</h1>
+        <section className='h-full container flex flex-col justify-center mx-auto space-y-10'>
+          <h1 className='text-6xl'>Update User</h1>
 
-        <div>
-          User info goes here
-        </div>
+          <div className="flex flex-col py-4 items-stretch space-y-7 mx-auto text-2xl">
+            <label className="flex flex-row place-content-between items-center space-x-2">
+              <div>Name:</div>
+              <input className='p-2 rounded-md'type='text'placeholder='.' value={nameT} onChange={e => setNameT(e.target.value)} />
+            </label>
+            <label className='flex flex-row place-content-between items-center space-x-2'>
+              <div>Username:</div>
+              <input className='p-2 rounded-md' type="text" placeholder='.' value={usernameT} onChange={e => setUsernameT(e.target.value)} />
+            </label>
+            <label className='flex flex-row place-content-between items-center space-x-2'>
+              <div>Password:</div>
+              <input className='p-2 rounded-md' type="password" value={passwordT} onChange={e => setPasswordT(e.target.value)} />
+            </label>
+          </div>
+
+          <div className='flex flex-row space-x-4 text-2xl'>
+              <button className='p-4 w-1/2 border-2 rounded-full ease-in-out duration-300 hover:bg-light hover:text-eerieBlack' type="submit" onClick={LoginRegisterSwap}>
+                <div>
+                  Cancel
+                </div>
+              </button>
+              <button className='p-4 w-1/2 border-2 rounded-full ease-in-out duration-300 hover:bg-light hover:text-eerieBlack' type="submit" onClick={Register}>
+                <div>
+                  Update info
+                </div>
+              </button>
+          </div>
+        </section>
       </>
     )
   }
